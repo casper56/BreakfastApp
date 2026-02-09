43423602454d689063f7dd777cf2d88a69974053
casper56 <casper56.tu@gmail.com>
2026-02-09 14:02:42 +0800

    Adjust form width to 1080

--- Form1.cs
+++ Form1.cs
@@ -46,7 +46,7 @@ namespace BreakfastApp
         private void SetupDynamicUI()
         {
             this.Text = "早餐店點餐管理系統 (智慧選擇版) - v1.2";
-            this.Size = new Size(1050, 900); // 調整寬度至 1050
+            this.Size = new Size(1080, 900); // 調整寬度至 1080
             this.FormBorderStyle = FormBorderStyle.FixedSingle; // 禁止調整大小
             this.MaximizeBox = false; // 停用最大化按鈕
             this.StartPosition = FormStartPosition.CenterScreen;
